3a520b63df7f0adbc79f377939328236c1d8d085
soumyadeep2007 <soumyadeep.chakraborty@stonybrook.edu>
2017-10-13 18:38:30 -0400

    Updated experiment script

--- runBench.sh
+++ runBench.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-runBenchmark() {
+runBenchmarkFineGrained() {
 	BENCHMARK=$1
 	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~$BENCHMARK~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> report 2>&1
 	echo "Individual options----------------------------------------------" >> report 2>&1
@@ -18,14 +18,32 @@ runBenchmark() {
 		"-loop-unroll -mem2reg" \
 		"-bb-vectorize -gvn -licm" \
 		"-licm -loop-unroll" \
+		"-loop-rotate -loop-unroll" \
 		"-licm -indvars -loop-unroll" \
 		"-bb-vectorize -gvn -loop-rotate -loop-unroll" \
+		"-loop-rotate -loop-unroll" \
+		"-gvn -loop-rotate -loop-unroll" \
+		"-bb-vectorize -gvn -loop-rotate" \
 		 )
 	for option in "${optionsArray[@]}"
 	do
 		run "$option" -DSTANDARD_DATASET "$BENCHMARK"
 		run "$option" -DLARGE_DATASET "$BENCHMARK"
 	done
+}
+
+runBenchmarkCoarseGrained() {
+	FLAG=$1
+	SIZE=$2
+	BENCHMARK=$3
+	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> report 2>&1
+	echo "Running with:::::::::::::::::::::::::::::::::::::::::::::::::::" >> report 2>&1
+	echo $BENCHMARK $FLAG $SIZE >> report 2>&1
+	clang $FLAG -DPOLYBENCH_TIME $SIZE -I . ../utilities/polybench.c "./$BENCHMARK.c" -o bench.out -lm
+	./time_benchmark.sh ./bench.out >> report 2>&1
+	./cleanup.sh
+	echo ">>>>>>>>>>>>>>>>>>>>>>>--X-->>>>>>>>>>>>>>>>>>>>>>>" >> report 2>&1
+	echo >> report 2>&1
 }
 
 run() { 
@@ -45,8 +63,31 @@ run() {
 	echo >> report 2>&1
 }
 
-#Execution
-runBenchmark "jacobi-2d"
-runBenchmark "gramschmidt"
-runBenchmark "gemm"
-runBenchmark "correlation"
+Execution
+runBenchmarkFineGrained "jacobi-2d"
+runBenchmarkFineGrained "gramschmidt"
+runBenchmarkFineGrained "gemm"
+runBenchmarkFineGrained "correlation"
+
+runBenchmarkCoarseGrained "-O0" "-DSTANDARD_DATASET" "jacobi-2d"
+runBenchmarkCoarseGrained "-O0" "-DLARGE_DATASET" "jacobi-2d"
+runBenchmarkCoarseGrained "-O3" "-DSTANDARD_DATASET" "jacobi-2d"
+runBenchmarkCoarseGrained "-O3" "-DLARGE_DATASET" "jacobi-2d"
+runBenchmarkCoarseGrained "-O0" "-DSTANDARD_DATASET" "gramschmidt"
+runBenchmarkCoarseGrained "-O0" "-DLARGE_DATASET" "gramschmidt"
+runBenchmarkCoarseGrained "-O3" "-DSTANDARD_DATASET" "gramschmidt"
+runBenchmarkCoarseGrained "-O3" "-DLARGE_DATASET" "gramschmidt"
+runBenchmarkCoarseGrained "-O0" "-DSTANDARD_DATASET" "gemm"
+runBenchmarkCoarseGrained "-O0" "-DLARGE_DATASET" "gemm"
+runBenchmarkCoarseGrained "-O3" "-DSTANDARD_DATASET" "gemm"
+runBenchmarkCoarseGrained "-O3" "-DLARGE_DATASET" "gemm"
+runBenchmarkCoarseGrained "-O0" "-DSTANDARD_DATASET" "correlation"
+runBenchmarkCoarseGrained "-O0" "-DLARGE_DATASET" "correlation"
+runBenchmarkCoarseGrained "-O3" "-DSTANDARD_DATASET" "correlation"
+runBenchmarkCoarseGrained "-O3" "-DLARGE_DATASET" "correlation"
+
+
+runBenchmarkCoarseGrained "-O3" "-DEXTRALARGE_DATASET" "jacobi-2d"
+runBenchmarkCoarseGrained "-O3" "-DEXTRALARGE_DATASET" "gramschmidt"
+runBenchmarkCoarseGrained "-O3" "-DEXTRALARGE_DATASET" "gemm"
+runBenchmarkCoarseGrained "-O3" "-DEXTRALARGE_DATASET" "correlation"
